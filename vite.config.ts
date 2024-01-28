8c4e413c8f7a288faaf5b0ab1161a8427e4c0089
Kepi <kepi@igloonet.cz>
2024-01-28 20:53:46 +0100

    feat: try to resolve xss imports in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-import { defineConfig } from "vite";
-import pkg from "./package.json";
-import { qwikVite } from "@builder.io/qwik/optimizer";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-const { dependencies = {}, peerDependencies = {} } = pkg as any;
-const makeRegex = (dep) => new RegExp(`^${dep}(/.*)?$`);
-const excludeAll = (obj) => Object.keys(obj).map(makeRegex);
+import { defineConfig } from "vite"
+import { qwikVite } from "@builder.io/qwik/optimizer"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig(() => {
   return {
@@ -16,15 +11,7 @@ export default defineConfig(() => {
         formats: ["es", "cjs"],
         fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
       },
-      rollupOptions: {
-        // externalize deps that shouldn't be bundled into the library
-        external: [
-          /^node:.*/,
-          ...excludeAll(dependencies),
-          ...excludeAll(peerDependencies),
-        ],
-      },
     },
     plugins: [qwikVite(), tsconfigPaths()],
-  };
-});
+  }
+})
